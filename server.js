a0a9545fb039806fcc2e502f66f6b4975a1c715d
byu-ai <adalenh25@gmail.com>
2024-06-11 14:52:00 -0600

    FINALUpdate server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const askChatGPT = require('./puppeteer');
 
 const app = express();
 const port = process.env.PORT || 10000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/ask-chatgpt', async (req, res) => {
